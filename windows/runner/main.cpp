e32e0de5fd8ec22992475612be72b8c379ac7f75
Tux <tux@fiedler.live>
2026-03-18 09:34:51 +0100

    App name + logo

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(400, 300);
-  if (!window.Create(L"skypersky", origin, size)) {
+  if (!window.Create(L"Skypersky", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
